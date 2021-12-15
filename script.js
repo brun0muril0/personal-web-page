fd83f4e64cc7ed503b58f73df89c9e5d6c6b2f4c
brun0muril0 <brunomuriloc014@hotmail.com>
2021-12-15 12:21:03 -0300

    typing text animation

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        // if(this.scroll > 500){
+        //     $('.scroll-up-btn').addClass("show");
+        // }else{
+        //     $('.scroll-up-btn').removeClass("show");
+        // }
     });
+
+
+    // slide-up script
+    // $('.scroll-up-btn').click(function(){
+    //     $('html').animate({scrollTop: 0});
+    // });
+
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
@@ -13,6 +25,20 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
+    // typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Developer", "Freelancer", "IT Analyst"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Developer", "Freelancer", "IT Analyst"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
     //owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
